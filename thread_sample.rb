afc9114cce20dac8c8da45192485eace16abbc94
James Dela Cruz <delacruzjamesmartin@gmail.com>
2020-04-25 02:19:43 +0800

    Create sample thread

--- thread_sample.rb
+++ thread_sample.rb
@@ -1,13 +1,31 @@
-# t = Thread.new { puts 10**10 }
-# puts "hello"
-# t.join
+# # t = Thread.new { puts 10**10 }
+# # puts "hello"
+# # t.join
+#
+# #
+# # threads = []
+# # 10.times {
+# #   threads << Thread.new { puts 1 }
+# # }
+# # threads.each(&:join)
+#
+#
+# Thread.new { raise 'hell' }
 
-# 
-# threads = []
-# 10.times {
-#   threads << Thread.new { puts 1 }
-# }
-# threads.each(&:join)
 
+require 'concurrent'
 
-Thread.new { raise 'hell' }
+$stdout.sync = true
+
+pool = Concurrent::FixedThreadPool.new(1000)
+
+600851475143.times do |url|
+  pool.post {
+    puts "fetching url..."
+    sleep(1)
+    puts "Done."
+  }
+end
+
+pool.shutdown
+pool.wait_for_termination
